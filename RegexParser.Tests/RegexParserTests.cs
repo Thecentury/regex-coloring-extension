146c2e35b4c766e9f7a1a4082ca46a23ae56b311
Thecentury <thecentury@gmail.com>
2013-12-08 22:59:45 +0400

    Parsing verbatim strings

---
+++ RegexParser.Tests/RegexParserTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace RegexParsing.Tests
+{
+	[TestFixture]
+	public sealed class RegexParserTests
+	{
+		[TestCase( "abc" )]
+		public void ShouldParse( string regex )
+		{
+			var tokens = RegexParser.ParseRegex( regex );
+		}
+	}
+}
