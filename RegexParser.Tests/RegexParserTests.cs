e751327c93c010e1444b28cb7aa87f937f0e9f3d
Thecentury <thecentury@gmail.com>
2013-12-10 23:16:46 +0400

    Implemented exclude in lists

--- RegexParser.Tests/RegexParserTests.cs
+++ RegexParser.Tests/RegexParserTests.cs
@@ -10,12 +10,24 @@ namespace RegexParsing.Tests
 		[TestCase( "[a-b]" )]
 		[TestCase( "[a-b23]" )]
 		[TestCase( "[a-b23-5]" )]
+		[TestCase( "[^a-b23-5]" )]
 		public void ShouldParse( string regex )
 		{
 			var tokens = RegexParser.ParseRegex( regex );
 
 			string str = String.Join( ",", tokens );
 			Console.WriteLine( str );
+		}
+
+		[TestCase( "[^1-2]", true )]
+		[TestCase( "[1-2]", false )]
+		public void ShouldParseExcludeList( string regex, bool expectedExclude )
+		{
+			var tokens = RegexParser.ParseRegex( regex );
+
+			CharList firstToken = (CharList)tokens[0];
+
+			Assert.That( firstToken.Exclude, Is.EqualTo( expectedExclude ) );
 		}
 	}
 }
