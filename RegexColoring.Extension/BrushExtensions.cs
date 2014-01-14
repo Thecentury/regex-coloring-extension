c82689479f59a64f847ebb63572a15cb08cc6a1c
Mikhail Brinchuk <3943804+Thecentury@users.noreply.github.com>
2014-01-14 22:46:59 +0400

    Highlighting is now semi-transparent

---
+++ RegexColoring.Extension/BrushExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Media;
+
+namespace RegexColoring.Extension
+{
+	public static class BrushExtensions
+	{
+		public static SolidColorBrush WithTransparency( this SolidColorBrush brush, double transparency )
+		{
+			SolidColorBrush modifiableClone = brush;
+			if ( brush.IsFrozen )
+			{
+				modifiableClone = brush.Clone();
+			}
+
+			Color color = modifiableClone.Color;
+			color.A = (byte)( transparency * 255 );
+			modifiableClone.Color = color;
+
+			return modifiableClone;
+		}
+	}
+}
