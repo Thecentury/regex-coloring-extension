b8496e00fa34865d399f295ac3fee4acd4a13449
Mikhail Brinchuk <3943804+Thecentury@users.noreply.github.com>
2014-01-14 22:52:02 +0400

    Basic tooltips

---
+++ RegexColoring.Extension/Colorizer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Media;
+using RegexParsing;
+
+namespace RegexColoring.Extension
+{
+	public static class Colorizer
+	{
+		private const double DefaultTransparency = 0.4;
+
+		public static Brush GetBrushForToken( PrimitiveRegexTokenKind kind )
+		{
+			var brush = GetBrushForTokenCore( kind ).WithTransparency( DefaultTransparency );
+			return brush;
+		}
+
+		private static SolidColorBrush GetBrushForTokenCore( PrimitiveRegexTokenKind kind )
+		{
+			switch ( kind )
+			{
+				case PrimitiveRegexTokenKind.OpenSquareBracket:
+				case PrimitiveRegexTokenKind.CloseSquareBracket:
+				case PrimitiveRegexTokenKind.CharacterListNegation:
+					return Brushes.Aquamarine;
+				case PrimitiveRegexTokenKind.RepetitionsCount:
+					return Brushes.CornflowerBlue;
+				case PrimitiveRegexTokenKind.RangeStart:
+				case PrimitiveRegexTokenKind.RangeSymbol:
+				case PrimitiveRegexTokenKind.RangeEnd:
+				case PrimitiveRegexTokenKind.LiteralCharacter:
+					return Brushes.Orange;
+				case PrimitiveRegexTokenKind.LiteralString:
+					return Brushes.Transparent;
+				default:
+					throw new ArgumentOutOfRangeException( "kind" );
+			}
+		}
+	}
+}
